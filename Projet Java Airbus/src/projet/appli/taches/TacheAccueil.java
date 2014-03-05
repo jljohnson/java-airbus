6841539cb32276aa9995b5610628c89c36308712
Yoan <Yoan@130.120.209.138>
2014-03-05 09:40:35 +0100

    Tache accueil

--- Projet Java Airbus/src/projet/appli/taches/TacheAccueil.java
+++ Projet Java Airbus/src/projet/appli/taches/TacheAccueil.java
@@ -1,5 +1,14 @@
 package projet.appli.taches;
 
-public class TacheAccueil {
+import projet.appli.Tache;
+import projet.outils.Horaire;
+
+public class TacheAccueil extends Tache {
+	private static int numTache = 1 ;
+
+	public TacheAccueil(Horaire debut, Horaire fin) {
+		super("ACC" + numTache, debut, fin);
+		// TODO Auto-generated constructor stub
+	}
 
 }
