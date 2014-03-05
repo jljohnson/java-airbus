1e46d168e775fdb68460ffca01fe2f810f2d1409
StefanLM <stefan.lemoing@hotmail.fr>
2014-03-05 11:01:59 +0100

    Tache accueil et repas

--- Projet Java Airbus/src/projet/appli/taches/TacheRepas.java
+++ Projet Java Airbus/src/projet/appli/taches/TacheRepas.java
@@ -1,6 +1,15 @@
 package projet.appli.taches;
 
+import projet.appli.Tache;
+import projet.outils.Duree;
+import projet.outils.Horaire;
 
-public class TacheRepas {
 
+public class TacheRepas extends Tache{
+	private static int numTaches = 1 ;
+
+	public TacheRepas(Horaire horaireDeb) {
+		super("ACC" + numTaches ,horaireDeb, horaireDeb.ajout(new Duree(1,0))) ;
+		numTaches++;
+	}
 }
