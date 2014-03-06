6f54767a0a27fa6ac9a9f8b5b3c44825303cdeaf
Yoan <igloosentier@gmail.com>
2014-03-06 14:10:50 +0100

    dcddsdfdffgjhnxdfdgbvwxd

--- Projet Java Airbus/src/projet/ihm/FenetreGestion.java
+++ Projet Java Airbus/src/projet/ihm/FenetreGestion.java
@@ -1,6 +1,9 @@
 package projet.ihm;
 
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class FenetreGestion extends JFrame {
 	public FenetreGestion() {
@@ -9,5 +12,13 @@ public class FenetreGestion extends JFrame {
 		setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
 		setResizable(false); //On interdit la redimensionnement de la fenêtre
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu menuAgent = new JMenu("Agent");
+		
+		menuBar.add(menuAgent);
+		
+		this.setJMenuBar(menuBar);
 	}
 }
